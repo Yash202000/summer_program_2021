04d2fd86ae576f7edb4c2bbd23d891091fb4d87a
Yash202000 <69676333+Yash202000@users.noreply.github.com>
2021-09-03 15:50:02 +0530

    Delete Task-11/lib directory

--- Task-11/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'pages/HomePage.dart';
-
-main() {
-  return runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),
-  ));
-}
